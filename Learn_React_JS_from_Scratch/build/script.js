58ab5885c033ddbf6ea931d7f7f40d136b760af2
Dimos Fountoukos <dimosthenis.fountoukos@baesystems.com>
2017-06-02 15:47:21 +0100

    component lifecycle

--- Learn_React_JS_from_Scratch/build/script.js
+++ Learn_React_JS_from_Scratch/build/script.js
@@ -1,20 +1,107 @@
 var Courses = React.createClass({
+	statics: {
+		fooMethod: function (text) {
+			return text === 'Hi';
+		}
+	},
+	propTypes: {
+		favColour: React.PropTypes.string.isRequired
+	},
+	getInitialState: function () {
+		return { currentPhone: 'Samsung Galaxy S6 Edge' };
+	},
+	getDefaultProps: function () {
+		return { music: 'Rock',
+			favColour: 'blue' };
+	},
 	render: function () {
 		return React.createElement(
 			'span',
-			null,
-			'Hello ',
-			this.props.name,
-			'!'
+			{ className: 'content', 'data-fav-music': this.props.music },
+			React.createElement(
+				'p',
+				null,
+				'Hello ',
+				this.props.name,
+				' \u2122'
+			),
+			React.createElement(
+				'p',
+				null,
+				'Your current device is ',
+				this.state.currentPhone
+			)
 		);
 	}
 });
 
 var properties = {
 	name: 'Dimos',
-	music: 'trance'
+	music: 'Trance'
 };
 
 var coursesElement = React.createElement(Courses, properties);
 
-React.render(coursesElement, document.getElementById('container'));
+var Counter = React.createClass({
+	getInitialState: function () {
+		return { count: this.props.count };
+	},
+	componentWillMount: function () {
+		console.log('componentWillMount called');
+		if (this.props.count === 0) {
+			this.state.buttonStyle = { 'color': 'red' };
+		}
+	},
+	componentDidMount: function () {
+		console.log(ReactDOM.findDOMNode(this));
+		//this.increment = setInterval(this.update, 400)
+	},
+	componentWillUnmount: function () {
+		console.log('componentWillUnmount called');
+		//clearInterval(this.increment)
+	},
+	componentWillReceiveProps: function (nextProps) {
+		console.log('componentWillReceiveProps called');
+	},
+	shouldComponentUpdate: function (nextProps, nextState) {
+		console.log('shouldComponentUpdate called', nextProps, nextState);
+		return nextState.count % 2 === 0;
+	},
+	componentWillUpdate: function (nextProps, nextState) {
+		console.log('componentWillUpdate called', nextState, this.state);
+		return true;
+	},
+	componentDidUpdate: function (prevProps, prevState) {
+		console.log('componentDidUpdate called', prevState, this.state);
+		return true;
+	},
+	update: function () {
+		var clickCount = this.state.count + 1;
+		this.setState({ count: clickCount });
+	},
+	render: function () {
+		console.log('render called', this.state.count);
+		return React.createElement(
+			'p',
+			{ ref: 'pTag' },
+			'Times clicked ',
+			this.state.count,
+			' ',
+			React.createElement(
+				'button',
+				{ style: this.state.buttonStyle, onClick: this.update },
+				'update'
+			)
+		);
+	}
+});
+
+var clickCounterElement = React.createElement(Counter, { count: 0 });
+window.renderComponent = function () {
+
+	ReactDOM.render(clickCounterElement, document.getElementById('container'));
+};
+
+window.unmountComponent = function () {
+	ReactDOM.unmountComponentAtNode(document.getElementById('container'));
+};
--- Learn_React_JS_from_Scratch/src/script.js
+++ Learn_React_JS_from_Scratch/src/script.js
@@ -1,6 +1,24 @@
 var Courses = React.createClass({
+	statics: {
+		fooMethod: function(text){
+			return text === 'Hi'
+		}
+	},
+	propTypes:{
+		favColour: React.PropTypes.string.isRequired
+	},
+	getInitialState:function(){
+		return {currentPhone:'Samsung Galaxy S6 Edge'}
+	},
+	getDefaultProps:function(){
+		return {music:'Rock',
+				favColour:'blue'}
+	},
 	render: function(){
-		return (<span>Hello {this.props.name}!</span>)
+		return (<span className='content' data-fav-music={this.props.music} >
+			<p>Hello {this.props.name} &trade;</p>
+			<p>Your current device is {this.state.currentPhone}</p>
+			</span>)
 
 	}
 })
@@ -8,9 +26,61 @@ var Courses = React.createClass({
 
 var properties = {
 	name:'Dimos',
-	music: 'trance'
+	music: 'Trance'
 }
 
 var coursesElement = <Courses {...properties}/>
 
-React.render(coursesElement, document.getElementById('container'))
+var Counter = React.createClass({
+	getInitialState: function(){
+		return {count:this.props.count}
+	},
+	componentWillMount: function(){
+		console.log('componentWillMount called')
+		if(this.props.count === 0){
+			this.state.buttonStyle={'color':'red'}
+		}
+	},
+	componentDidMount: function(){
+		console.log(ReactDOM.findDOMNode(this))
+		//this.increment = setInterval(this.update, 400)
+	},
+	componentWillUnmount: function(){
+		console.log('componentWillUnmount called')
+		//clearInterval(this.increment)
+	},
+	componentWillReceiveProps: function(nextProps) {
+			console.log('componentWillReceiveProps called')
+	},
+	shouldComponentUpdate: function(nextProps,nextState){
+		console.log('shouldComponentUpdate called', nextProps, nextState)
+		return  nextState.count % 2 === 0 
+	},
+	componentWillUpdate: function(nextProps,nextState){
+		console.log('componentWillUpdate called', nextState, this.state)
+		return true
+	},
+	componentDidUpdate: function(prevProps,prevState){
+		console.log('componentDidUpdate called', prevState, this.state)
+		return true
+	},
+	update: function(){
+		var clickCount = this.state.count + 1
+		this.setState({count:clickCount})
+	},
+	render: function(){
+		console.log('render called', this.state.count)
+		return (<p ref='pTag'>Times clicked {this.state.count} <button style={this.state.buttonStyle} onClick={this.update}>update</button></p>)
+	}
+})
+
+var clickCounterElement = <Counter count={0}/>
+window.renderComponent = function(){
+	
+	ReactDOM.render(clickCounterElement, document.getElementById('container'))	
+}
+
+window.unmountComponent = function(){
+	ReactDOM.unmountComponentAtNode(document.getElementById('container'))
+}
+
